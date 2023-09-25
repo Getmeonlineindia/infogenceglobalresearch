aef5c3625f62f864b74d385a0093777d0246875a
Raviegupta <raviegupta153@gmail.com>
2023-09-25 17:16:22 +0530

    JS: Adding JS Files for  MOBILE MENU HAMBURGER  ICON, HOMEPAGE BANNER SLIDER, HOMEPAGE CATEGORIES SLIDER

--- js/mobile-menu-hamburger-icon.js
+++ js/mobile-menu-hamburger-icon.js
@@ -1,6 +1,8 @@
+// THIS JAVASCRIPT FILE IS FOR HAMBURGER ICON IN MOBILE MENU
+
 const menuList = document.getElementById('menu-list');
         
-        // menuList.style.maxHeight = "0px";
+// menuList.style.maxHeight = "0px";
 
 function togglemenu() {
     if (menuList.classList.contains('hidden')) {
--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,5 @@
+// THIS JAVASCRIPT FILE IS FOR HOMEPAGE SLIDER FOR CATEGORIES SECTION
+
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
